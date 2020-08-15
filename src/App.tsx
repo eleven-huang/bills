3c15c2f70c79fbeba95c7f226b580caba8c3df2f
yijun <babykaokao@gmail.com>
2020-08-16 00:02:45 +0800

    load data

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Bills from './bills/bills'
+
+function App() {
+  return (
+    <div className="App">
+      <Bills />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/bills/bills.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import * as d3 from 'd3-fetch'
+
+type BillData = {
+  time: string,
+  type: number,
+  category: string,
+  amount: number,
+}
+
+type CategoryData = {
+  id: string,
+  type: number,
+  name: string
+}
+
+type Props = {
+
+}
+
+type State = {
+  categories: CategoryData[]
+  bills: BillData[],
+}
+
+
+export default class Bills extends React.Component<Props, State> {
+    public readonly state: Readonly<State> = {
+      bills: [],
+      categories: []
+    }
+
+    componentDidMount() {
+      getData(this);
+    }
+
+    render(): JSX.Element {
+      const listBills = this.state.bills.map((bill: BillData, index: number) => {
+          const category = getCategoryById(this.state.categories, bill.category);
+
+          return(
+            <div key={index}>
+              <span>{bill.amount}</span>
+              <span>{category.name}</span>
+            </div>
+          )
+      });
+
+      return (
+        <div>{listBills}</div>
+      )
+    }
+}
+
+function getData(billsComponent: React.Component<Props, State>) {
+  //get categories first
+  var csv_file_path = require("./data/bill_categories.csv");
+  d3.csv(csv_file_path).then(function(data) {
+    billsComponent.setState({categories: data})
+    getBills(billsComponent);
+  });
+}
+
+function getBills(billsComponent: React.Component<Props, State>) {
+  var csv_file_path = require("./data/bills.csv");
+  d3.csv(csv_file_path).then(function(data) {
+    billsComponent.setState({bills: data})
+  });
+}
+
+function getCategoryById(categories: CategoryData[], id: string): CategoryData {
+  const category: CategoryData | undefined = categories.find((category: CategoryData) => category.id === id);
+  if (category === undefined) {
+      throw new Error("Wrong categories or bills data");
+  }
+
+  return category;
+}
