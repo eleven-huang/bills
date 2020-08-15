e5b0aa4908d4c5bdd38beaf11694a14e7c808af4
yijun <babykaokao@gmail.com>
2020-08-16 02:35:56 +0800

    create Header for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Bills from './bills/bills'
+import Header from './header/header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Bills />
     </div>
   );
---
+++ src/header/header.tsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import {Navbar, Nav} from 'react-bootstrap'
+
+const Header: React.FunctionComponent = (): JSX.Element => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/">
+        <img
+          alt=""
+          src="/favicon.ico"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        BILLS
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link href="/">home</Nav.Link>
+        </Nav>
+
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Header;
